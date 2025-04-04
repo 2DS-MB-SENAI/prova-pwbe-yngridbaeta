a91b9cc55eb7fbe8dad118ee54efb7650f28a1ef
Yngrid Vit�ria Sa Baeta <40718940830@>
2025-04-04 09:32:18 -0300

    adicionando cardiologista

--- clinica/models.py
+++ clinica/models.py
@@ -4,7 +4,8 @@ from django.db import models
 ESPECIALIDADES = [
     ('Cirurgiao', 'Cirurgiao'), 
     ('Oftalmologista', 'Oftalmologista'),
-    ('Enfermeiro', 'Enfermeiro')
+    ('Enfermeiro', 'Enfermeiro'),
+    ('CAR', 'Cadiologista')
 ]
 
 STATUS = [
