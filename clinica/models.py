542d13eccbe1fc5badab1c7902b59b1538ed8cf9
Yngrid Vit�ria Sa Baeta <40718940830@>
2025-04-04 08:26:16 -0300

    Criando modelos

--- clinica/models.py
+++ clinica/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+ESPECIALIDADES = [
+    ('Cirurgiao', 'Cirurgiao'), 
+    ('Oftalmologista', 'Oftalmologista'),
+    ('Enfermeiro', 'Enfermeiro')
+]
+
+STATUS = [
+    ('agendado', 'agendado'),
+    ('realizado', 'realizado'),
+    ('cancelado', 'cancelado')
+]
+
+class Medico(models.Model):
+    nome = models.CharField(max_length=50)
+    especialidade = models.CharField(choices=ESPECIALIDADES, max_length=30)
+    crm = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(blank=True)
+
+class Consulta(models.Model):
+    pacientes = models.CharField(max_length=100)
+    data = models.DateTimeField()
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    status = models.CharField(choices=STATUS, max_length=30)
